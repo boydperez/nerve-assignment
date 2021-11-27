0b33a67a80e29f6a34c565c7dd6dba63d4d180e7
Boyd <boydpires@gmail.com>
2021-11-27 11:25:00 +0530

    Update index content

--- models/record-model.js
+++ models/record-model.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const recordSchema = new mongoose.Schema({
     srNo: {
         type: String,
-        required: [true, 'Record needs to have a Serial number'],
+        required: [true, 'Record needs to have a Serial number (srNo)'],
         unique: true,
     },
     name: {
         type: String,
-        required: [true, 'Record needs to have a name'],
+        required: [true, 'Record needs to have a name (name)'],
         trim: true,
     },
 });
